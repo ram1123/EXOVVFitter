9d8d60d3b4a8db3642c3c9625dda2b03cf1fcfbd
lbrianza <lucabrianza@gmail.com>
2016-03-14 10:33:29 +0100

    update

--- run-all.py
+++ run-all.py
@@ -4,22 +4,24 @@ from optparse import OptionParser
 
 parser = OptionParser()
 
-parser.add_option('-c', '--channel',action="store",type="string",dest="channel",default="el")
-parser.add_option('-s', action="store",type="string",dest="sample",default="BulkG_WW")
+parser.add_option('-c', '--channel',action="store",type="string",dest="channel",default="mu")
+parser.add_option('-s', action="store",type="string",dest="sample",default="BulkGraviton_newxsec")
 parser.add_option('--category', action="store",type="string",dest="category",default="HP")
-parser.add_option('--jetalgo', action="store",type="string",dest="jetalgo",default="Mjpruned")
+parser.add_option('--jetalgo', action="store",type="string",dest="jetalgo",default="jet_mass_pr")
 parser.add_option('--interpolate', action="store_true",dest="interpolate",default=False)
 parser.add_option('--batchMode', action="store_true",dest="batchMode",default=False)
 (options, args) = parser.parse_args()
 
 currentDir = os.getcwd();
 
+masses = [600,700,750,800,900,1000]
 #masses = [800,1200,1400,1600,1800,2000,2500,3000,3500,4000,4500]
-masses = [800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000,
-          3100,3200,3300,3400,3500,3600,3700,3800,3900,4000,4100,4200,4300,4400,4500] #use this one only for the interpolation mode!
+#masses = [800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000,
+#          3100,3200,3300,3400,3500,3600,3700,3800,3900,4000,4100,4200,4300,4400,4500] #use this one only for the interpolation mode!
+
 #masses = [3000,3500,4000,4500]
 for m in masses:
-   if (options.interpolate==True and options.batchMode==True):
+   if (options.interpolate==False and options.batchMode==True):
 
       fn = "Job/Job_%s_%s_%d.sh"%(options.channel,options.category,m)
       outScript = open(fn+".sh","w");
@@ -31,7 +33,7 @@ for m in masses:
       outScript.write("\n"+'echo ${PATH}');
       outScript.write("\n"+'ls');
 #      cmd = "python g1_exo_doFit_class.py -b -c %s --mass %i --category %s --sample %s_lvjj --jetalgo %s --interpolate True > log/%s_M%i_%s_%s.log" %(options.channel,m,options.category,options.sample,options.jetalgo,options.sample,m,options.channel,options.category)
-      cmd = "python g1_exo_doFit_class.py -b -c %s --mass %i --category %s --sample %s_lvjj --jetalgo %s --interpolate True" %(options.channel,m,options.category,options.sample,options.jetalgo)
+      cmd = "python g1_exo_doFit_class_new.py -b -c %s --mass %i --category %s --sample %s --jetalgo %s > log/%s_M%i_%s_%s.log" %(options.channel,m,options.category,options.sample,options.jetalgo,options.sample,m,options.channel,options.category)
       outScript.write("\n"+cmd);
 #      outScript.write("\n"+'rm *.out');
       outScript.close();
